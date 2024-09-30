805c22ff631117451c11ee2ff6f18ba92d6d8c19
Giovanni Perniola <gioperniola1999@gmail.com>
2024-09-30 17:33:20 +0200

    terminato selfwork operatori 2

---
+++ main.js
@@ -0,0 +1,9 @@
+let year = 2024;
+let birthYear = 1999;
+
+let age = year - birthYear;
+let yearsLeft = 100 - age;
+
+console.log(`Hai ${age} anni e ti mancano ${yearsLeft} anni per compierne 100`);
+
+
